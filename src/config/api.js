57ab20d6453fecc5b99da2f47933569b4a2bd3df
Your Name <samsulr99@gmail.com>
2022-05-10 10:25:10 +0700

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const API = () => {
   const baseUrl =
-    process.env.SERVER_URL ||
+    process.env.REACT_APP_SERVER_URL ||
     'https://dumbmerch-b33.herokuapp.com/api/v1' ||
     'https://localhost:5000/api/v1';
 
